469db9fcc84a66783e7a5c80f48a0434d07290bc
tyshchukRoman <roman.t@codelibry.dev>
2025-06-02 23:43:19 +0300

    add integration with WPML

--- helpers/cp_get_current_urls.php
+++ helpers/cp_get_current_urls.php
@@ -1,17 +1,58 @@
 <?php
 
 function cp_get_current_urls() {
-  $post_type = get_option('compare_permalinks_post_types', 'any');
+    $post_type = get_option('compare_permalinks_post_types', 'any');
+    $all_urls = [];
 
-  $args = [
-    'post_type'       => $post_type,
-    'post_status'     => 'publish',
-    'posts_per_page'  => -1,
-    'orderby'         => 'title',
-    'order'           => 'ASC',
-  ];
+    /*
+     * WPML active
+     */
+    if (function_exists('icl_get_languages')) {
+        $languages = icl_get_languages('skip_missing=0');
+        $original_lang = apply_filters('wpml_current_language', null);
 
-  $posts = get_posts($args);
+        foreach ($languages as $lang_code => $lang) {
+            do_action('wpml_switch_language', $lang_code);
 
-  return array_map(fn($post) => get_permalink($post), $posts);
+            $args = [
+                'post_type'      => $post_type,
+                'post_status'    => 'publish',
+                'posts_per_page' => -1,
+                'orderby'        => 'title',
+                'order'          => 'ASC',
+                'suppress_filters' => false,
+            ];
+
+            $posts = get_posts($args);
+
+            foreach ($posts as $post) {
+                $all_urls[] = apply_filters('wpml_permalink', get_permalink($post), $lang_code);
+            }
+        }
+
+        // Switch back to original language
+        do_action('wpml_switch_language', $original_lang);
+    } 
+    
+    /*
+     * No Multillingual plugins enabled
+     */
+    else {
+        $args = [
+            'post_type'         => $post_type,
+            'post_status'       => 'publish',
+            'posts_per_page'    => -1,
+            'orderby'           => 'title',
+            'order'             => 'ASC',
+            'suppress_filters'  => false,
+        ];
+
+        $posts = get_posts($args);
+
+        foreach ($posts as $post) {
+            $all_urls[] = get_permalink($post);
+        }
+    }
+
+    return array_unique($all_urls);
 }
